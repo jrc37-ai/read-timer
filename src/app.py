fa7ee033ee89ef0c555afd7bee5acfebb5cbfd3b
jrc37-ai <faraon.dnx@gmail.com>
2024-05-22 17:05:24 -0700

    Update with title adition

--- src/app.py
+++ src/app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/editar")
+def editar():
+    return render_template('editar.html')
+
+@app.route("/nuevolibro")
+def nuevo_libro():
+    return render_template('nuevo-libro.html')
+
+@app.route("/leyendo", methods=['POST'])
+def leyendo():
+    titulo = request.form['book-title']
+    return render_template('index.html', resultado=f'El título del libro es: "{titulo}"')
+
 if __name__=="__main__":
     app.run(debug=True)
